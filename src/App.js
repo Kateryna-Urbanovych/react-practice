d246f44ed0fbe86209fd42b04be34e6781810eca
Kateryna-Urbanovych <katya_urb@ukr.net>
2020-12-22 23:41:31 +0200

    добавляет стартовые файлы

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import AppBar from './components/AppBar/AppBar';
+import Container from './components/Container/Container';
+
+export default function App() {
+    return (
+        <Container>
+            <AppBar />
+        </Container>
+    );
+}
---
+++ src/components/AppBar/AppBar.js
@@ -0,0 +1,11 @@
+// import Navigation from '../Navigation/Navigation';
+import styles from './Appbar.module.css';
+
+export default function Appbar() {
+    return (
+        <header className={styles.header}>
+            Header
+            {/* <Navigation /> */}
+        </header>
+    );
+}
